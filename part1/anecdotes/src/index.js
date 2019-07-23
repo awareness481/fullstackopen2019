0bce7bf4eff298ccd3df18540c334f015d9e1af2
Jesse Jafa <jesse.jafa@gmail.com>
2019-07-23 12:11:00 +0300

    .

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 
 const anecdotes = [
   'If it hurts, do it more often',
