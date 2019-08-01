48085f2bfa241155abd87f01ea979026e257367b
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-01 12:34:27 +0300

    handled promise

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,33 +1,54 @@
 import React, { useState } from 'react'
+import apiService from './services/api';
 
 import PersonForm from './PersonForm';
 import SearchFilter from './SearchFilter';
+import Notification from './Notification';
+
+
+const { getAll, create, update } = apiService;
 
 const App = () => {
 
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]); 
+  ]);
+  const [ message, setMessage ] = useState(null);
 
   const checkDuplicate = (newName) => {
-    return persons.filter(person => person.name === newName).length > 0;
+    return getAll()
+      .then(res => res.data)
+      .then(res => (res.filter(person => person.name === newName)))
+      .then(res => (res.length > 0))
   }
+  
 
-  const handleNameSubmit = (e, newName, newPhone) => {
+  const handleNameSubmit = async (e, newName, newPhone) => {
     e.preventDefault();
 
-    if (checkDuplicate(newName)) {
-      alert(`${newName} already exists`);
-      return 'duplicate error'
+    if (await checkDuplicate(newName)) {
+      if (window.confirm('Name already exists. Replace number?')) {
+        const copy = [...persons];
+        copy[newName] = {
+          name: newName,
+          phone: newPhone,
+        }
+        update(newName, { name: newName, phone: newPhone});
+        setMessage(`Updated number for ${newName}`);
+        setPersons(copy)
+      }
+      return;
     }
-
+    setMessage(`Added ${newName} to the database!`);
     const copy = [...persons];
     copy.push({name: newName, phone: newPhone});
+    create({name: newName, phone: newPhone});
     setPersons(copy);
   }
 
   return (
     <div>
+      <Notification message={message} />
       <h2>Phonebook</h2>
       <PersonForm persons={persons} submit={handleNameSubmit}/>
       <SearchFilter persons={persons} />
