600fb3aa519071e8f879722f360ca37fed3e439a
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-11 05:32:28 +0300

    helper functions for tests

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, e) => acc + e.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const max = Math.max(...blogs.map(e => e.likes), 0);
+  return blogs.filter(e => e.likes === max)[0];
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
