6930f5c98dca573b179926eaeeb7c8be8242b503
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-14 06:51:09 +0300

    Fixed POST request, added PUT, DELETE operations

--- part4/controllers/appRouter.js
+++ part4/controllers/appRouter.js
@@ -11,9 +11,37 @@ appRouter.get('/', async (request, response) => {
 });
 
 appRouter.post('/', async (request, response) => {
-  const blog = await blog.save(new Blog(request.body))
+  const { title, url} = request.body;
 
-  response.status(201).json(blog);
+  const blog = new Blog(request.body);
+
+  if (!title && !url) {
+    response.status(400).send();
+    return;
+  }
+
+  const blogObject = await blog.save(blog);
+
+  response.status(201).json(blogObject);
 });
+
+appRouter.put('/:id', async (request, response, next) => {
+  const { likes } = request.body;
+  const { id } = request.params;
+
+  const blog = {
+    likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, { new: true })
+  response.json(updatedBlog.toJSON())
+})
+
+appRouter.delete('/:id', async (request, response, next) => {
+  
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end()
+
+})
 
 module.exports = appRouter;
