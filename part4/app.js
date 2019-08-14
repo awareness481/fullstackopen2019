22352d63af1a3620eb9fe47c5bec4e1b7c6812d4
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-14 06:04:45 +0300

    exported logger logic from app.js

--- part4/app.js
+++ part4/app.js
@@ -6,15 +6,18 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const appRouter = require('./controllers/appRouter');
 const { MONGODB_URI } = require('./utils/config');
+const logger = require('./utils/logger')
 
+
+// logger.info('connecting to', MONGODB_URI)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+  // .then(() => {
+  //   logger.info('connected to MongoDB')
+  // })
+  // .catch((error) => {
+  //   logger.error('error connection to MongoDB:', error.message)
+  // })
 
 app.use(cors())
 app.use(bodyParser.json())
