013298d334e9f7ac97a6d4d1629319c7e15de0d5
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-15 07:26:20 +0300

    uncommented logger code

--- part4/app.js
+++ part4/app.js
@@ -9,15 +9,15 @@ const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger')
 
 
-// logger.info('connecting to', MONGODB_URI)
+logger.info('connecting to', MONGODB_URI)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false })
-  // .then(() => {
-  //   logger.info('connected to MongoDB')
-  // })
-  // .catch((error) => {
-  //   logger.error('error connection to MongoDB:', error.message)
-  // })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
