935b5682cf9ae5d47a100ac755b7ec355e90c8f0
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-17 06:52:47 +0300

    token middleware, loginrouter

--- part4/app.js
+++ part4/app.js
@@ -8,10 +8,13 @@ const mongoose = require('mongoose')
 // UTILS
 const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger')
+const getTokenFrom = require('./utils/token')
+
 
 // ROUTERS
 const appRouter = require('./controllers/appRouter');
 const usersRouter = require('./controllers/usersRouter')
+const loginRouter = require('./controllers/login');
 
 
 logger.info('connecting to', MONGODB_URI)
@@ -27,8 +30,10 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use(getTokenFrom);
 app.use('/api/blogs', appRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 module.exports = app;
