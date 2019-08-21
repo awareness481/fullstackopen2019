07b231b3cf4257de5f58cc8688bd42ecc3935386
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-21 04:51:53 +0300

    Notification component & css initial

---
+++ part5/src/components/Notification.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/message.css';
+
+const Notification = ({ message, error = false }) => {
+  console.log(message)
+  if (message === null) {
+    return null
+  }
+
+  if (error === true) {
+    return (
+      <div className="error hide">
+        {message}
+      </div>
+    )
+  }
+
+  return (
+    <div className="success hide">
+      {message}
+    </div>
+  )
+}
+
+export default Notification;
