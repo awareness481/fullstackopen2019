6d3ec2bf58001e595ef3248292e247e73b9e0211
Jesse Jafa <jesse.jafa@gmail.com>
2019-08-29 11:22:25 +0300

    Added styles

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState} from 'react';
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+
+const Blog = ({ blog }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const visibility = toggle 
+    ? { display: 'block' }
+    : { display: 'none' }
+
+  return (
+  <div style={blogStyle}  onClick={() => setToggle(!toggle)}>
+    {blog.title}
+    <div style={visibility}>
+      <ul>
+        <li>{blog.author}</li>
+        <li><a href={blog.url}>Open Blog</a></li>
+        <li>{blog.likes}</li>
+      </ul>
+    </div>
   </div>
-)
+  )
+
+}
 
 export default Blog
